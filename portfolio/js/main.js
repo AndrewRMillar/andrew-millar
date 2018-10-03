c22018bde928925c5ef5ceb4aa453aa57b89bab2
Andrew Millar <duckman.andrew@gmail.com>
2018-10-03 09:45:05 +0200

    fixed the function that displayes a different quote
    was a set timout needed to be a settimeinterval....

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -21,10 +21,8 @@ const quotes = [
     "<q>If debugging is the process of removing software bugs, then programming must be the process of putting them in – <cite>Edsger Dijkstra</cite></q>"
 ]
 
-window.setTimeout(() => {
-    console.log("quote");
-    var random = Math.round(Math.random() * quotes.length);
-    random = random > 4? random: 4;
+window.setInterval(() => {
+    var random = Math.floor(Math.random() * quotes.length);
     var quote = quotes[random];
     document.querySelector(".quote h2").innerHTML = quote;
 }, 20000);
