13642a62a891a6881478de3ecc44990c6f691a6d
Andrew Millar <duckman.andrew@gmail.com>
2018-10-05 16:49:22 +0200

    Get script up to date

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,10 +1,11 @@
 // Paralax effect, not working so far. On the back burner
 window.addEventListener("scroll", () => {
     // get the scroll amount and move the backgroung image by a fraction of that amount
+    // For some reason this doesn't work, moving on coming 
 
-    let scrollFraq = window.scrollY / 4;
-    const header = document.querySelector("header");
-    header.style.backgoundPositionY = `0, ${scrollFraq}px`;   
+//     let scrollFraq = window.scrollY / 4;
+//     const header = document.querySelector("header");
+//     header.style.backgoundPositionY = `0, ${scrollFraq}px`;   
 
     if(window.innerHeight/window.scrollY < 3) {
         document.body.classList.add("scrolled");
@@ -12,28 +13,37 @@ window.addEventListener("scroll", () => {
 });
 
 // fetch a json file with the quotes
-const quotes = {};
-const url = "http://andrew-millar.nl/quotes.json";
+// fetch polyfill: http://github.github.io/fetch/
+let quotes = {};
+const url = "https://millar-knorr.nl/quotes.json";
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); 
-        quotes = data;
-    })
-    .catch(e => console.log(e));
+fetch(url, {
+    method: "POST",
+    mode: "no-cors"})
+.then(response => response.json())
+.then(data => {
+    quotes = data;
+    pastQuote();
+})
+.catch(e => console.log(e));
 
-// Sort of random quote generation, 
+function pastQuote () {
+    // Select a random quote from the quotes object 
+    var random = Math.floor(Math.random() * quotes.quotes.length);
+    var quote = quotes.quotes[random];
+    document.querySelector(".quote").innerHTML = `<h2>${quote}</h2>`;
+}
+// Change the quote every so often
 window.setInterval(() => {
-    var random = Math.floor(Math.random() * quotes.length);
-    var quote = quotes[random];
-    document.querySelector(".quote h2").innerHTML = `<h2>${quote}</h2>`;
-}, 10000);
+    pastQuote();
+}, 20000);
 
+
+// Setup for the scrollIntoView functionality
 const section = document.querySelectorAll("section");
 const next = document.querySelectorAll(".next");
 const last = document.querySelectorAll(".last");
-const ids = ["top"];
+const ids = ["top"]; // first id is not of a section but a header
 
 // Put all the section ids in the array
 section.forEach((el) => {
@@ -54,8 +64,7 @@ function scrollTo(el, direction) {
     })
 }
 
-// Lot DRYer than it was, could put the strings in an array and use forEach on that array and 
-// then a forEach inside every forEach iteration but I souldn't go overboard.
+// Lot DRYer than it was, don't go overboard.
 last.forEach((el) => {
     scrollTo(el, "up");
 });
