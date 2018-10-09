90c6640a2da917d1438532e631fe205604bc653e
Andrew Millar <duckman.andrew@gmail.com>
2018-10-09 17:27:16 +0200

    update
    Overhoaled the portfolio [art of the website

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -30,15 +30,15 @@ window.addEventListener("scroll", () => {
 let quotes = {};
 const url = "https://millar-knorr.nl/quotes.json";
 
-fetch(url, {
-    method: "POST",
-    mode: "no-cors"})
-.then(response => response.json())
-.then(data => {
-    quotes = data;
-    pastQuote();
-})
-.catch(e => console.log(e));
+// fetch(url, {
+//     method: "POST",
+//     mode: "no-cors"})
+// .then(response => response.json())
+// .then(data => {
+//     quotes = data;
+//     pastQuote();
+// })
+// .catch(e => console.log(e));
 
 function pastQuote () {
     // Select a random quote from the quotes object 
@@ -90,3 +90,44 @@ next.forEach((el) => {
     scrollTo(el, "down");
 });
 
+// Carousel code
+// when the...
+const carouselItems = document.querySelectorAll(".car");
+const lastBtn = document.querySelector(".lastt");
+const nextBtn = document.querySelector(".nextt");
+const classes = [];
+let activeItem = document.querySelector(".active");
+let targetIndex = 0;
+
+carouselItems.forEach((item) => {
+  // Get an array with second classnames
+  classes.push(item.classList[1]);
+});
+
+function changeActiveItem(dir) {
+  // Function takes in the direction detects what is the active 
+  // item then determins the next item repending on the direction 
+  // and changes the active item to that item
+  
+  activeItem = document.querySelector(".active");
+  itemClass = activeItem.classList[1];
+  targetIndex = classes.indexOf(itemClass);
+  if(dir == "up") {
+    targetIndex == classes.length-1? targetIndex = 0: targetIndex++;
+  } else {
+    targetIndex == 0? targetIndex = classes.length-1: targetIndex--;
+  }
+  activeItem.classList.remove("active");
+  carouselItems[targetIndex].classList.add("active");
+}
+
+lastBtn.addEventListener("click", () => {
+  changeActiveItem("down");  
+}); 
+nextBtn.addEventListener("click", () => {
+  changeActiveItem("up");  
+});
+
+// There needs to be a transition, something like a timeout on the button press 
+// that adds a class to the leaving active item and one to the new active item 
+// and removes them after the transition is finished, say 300ms
